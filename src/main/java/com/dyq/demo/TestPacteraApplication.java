9b389542a68fabdabde723014e6dc2ae6d13b047
dengyouquan <1257207999@qq.com>
2018-03-07 19:39:39 +0800

    test

---
+++ src/main/java/com/dyq/demo/TestPacteraApplication.java
@@ -0,0 +1,12 @@
+package com.dyq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPacteraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPacteraApplication.class, args);
+	}
+}
